507a2d6f7efaad6b4b756f0aa30fcc8a1fb2496e
zaid6isthename-bit <zaid6isthename@gmail.com>
2026-04-08 00:08:06 +0530

    feat: integrate Electron with concurrent development scripts and main process entry point

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
